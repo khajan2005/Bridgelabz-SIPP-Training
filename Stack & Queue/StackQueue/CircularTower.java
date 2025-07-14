e5fdbf435d46f380d5c624e45210793257b58c89
khajan chaudhary <khajanchaudhary763@gmail.com>
2025-07-14 11:49:58 +0530

    Add CircularTour implementation to find the starting petrol pump in a circular tour

---
+++ Stack & Queue/StackQueue/CircularTower.java
@@ -0,0 +1,26 @@
+class CircularTour {
+    static class PetrolPump {
+        int petrol;
+        int distance;
+
+        PetrolPump(int petrol, int distance) {
+            this.petrol = petrol;
+            this.distance = distance;
+        }
+    }
+
+    public static int findStartingPoint(PetrolPump[] pumps) {
+        int start = 0, deficit = 0, balance = 0;
+
+        for (int i = 0; i < pumps.length; i++) {
+            balance += pumps[i].petrol - pumps[i].distance;
+            if (balance < 0) {
+                start = i + 1;
+                deficit += balance;
+                balance = 0;
+            }
+        }
+
+        return (balance + deficit >= 0) ? start : -1;
+    }
+}
