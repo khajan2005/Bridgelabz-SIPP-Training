674be326f360ab1fe7306fc67f61270a606741ff
khajan chaudhary <khajanchaudhary763@gmail.com>
2025-07-15 09:37:01 +0530

    Create ReverseString.java

---
+++ searching/Bufferclasses/ReverseString.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class ReverseString {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine();
+		StringBuilder sb=new StringBuilder();
+		for(int i=s.length()-1;i>=0;i--) {
+			sb.append(s.charAt(i));
+		}
+		System.out.print(sb.toString());
+	}
+
+}
